707aea3a82a7e4b50eb5b2ec740dd440fa44479c
Everest Gjonaj <ogodeverest@192.168.1.6>
2022-11-13 12:16:55 +0100

    Changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/multi-step-checkout-form/",
   plugins: [react()],
   resolve: {
     alias: {
